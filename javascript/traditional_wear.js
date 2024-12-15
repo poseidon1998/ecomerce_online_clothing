e91b9c5374ee1577396e15fe6e3491e21a6b89e3
sasi <sasikumar.49822@gmail.com>
2024-12-16 02:59:14 +0530

    shirt updates

--- javascript/traditional_wear.js
+++ javascript/traditional_wear.js
@@ -1,9 +1,9 @@
 const products = [
-  { id: 1, name: "Men's Wear 1", price: 4000, discount: 1999, image: "img/black_shirt.PNG" },
-  { id: 2, name: "Men's Wear 2", price: 3500, discount: 1800, image: "img/green_shirt.PNG " },
-  { id: 3, name: "Men's Wear 3", price: 1599, discount: 899, image: "img/blue_shirt.PNG" },
-  { id: 4, name: "Men's Wear 4", price: 1000, discount: 599, image: "img/rd_shirt.PNG" },
-  { id: 5, name: "Men's Wear 5", price: 2500, discount: 1599, image: "img/black_shirt.PNG" }
+  { id: 1, name: "Men's Black Shirt", price: 4000, discount: 1999, image: "img/black_shirt.PNG" },
+  { id: 2, name: "Men's Blue Shirt", price: 3500, discount: 1800, image: "img/green_shirt.PNG " },
+  { id: 3, name: "Men's Blue Shirt", price: 1599, discount: 899, image: "img/blue_shirt.PNG" },
+  { id: 4, name: "Men's Maroon Shirt", price: 1000, discount: 599, image: "img/rd_shirt.PNG" },
+  { id: 5, name: "Men's Red Shirt", price: 2500, discount: 1599, image: "img/red_shirt.PNG" }
 ];
 
 const recipientNumber = "919003581835";
